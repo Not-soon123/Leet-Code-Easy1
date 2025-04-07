731bbb5d8a80e2912f5b081d3d1a7498bb2b2b50
Not-soon123 <natantibebu326@gmail.com>
2025-04-07 16:41:54 +0300

    Update #Two sum.py

--- #Two sum.py
+++ #Two sum.py
@@ -4,3 +4,14 @@ class Solution:
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j]==target:
                     return [i,j]
+#the optimized solution 
+    class Solution:
+        def twoSum(self, nums: List[int], target: int) -> List[int]:
+            dict1={}
+            for index, value in enumerate(nums):
+                val= target - value
+                if val in dict1:
+                    return [dict1[val], index]
+                else:
+                    dict1[value]=index
+            return []
